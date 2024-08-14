f6c0c506590a0ff90e0571f8d71ee37f5b741c79
zhangyuan <zhangyuan@users.noreply.github.com>
2024-08-14 21:07:25 +0800

    refactor and add authentication

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/gliderlabs/ssh"
 	"github.com/google/shlex"
@@ -29,56 +30,123 @@ func (reader SshIn) Read(data []byte) (int, error) {
 	return reader.session.Read(data)
 }
 
-func runGitReceivePackCmd(session ssh.Session, repoPath string) error {
-	cmd := exec.Command(GIT_RECEIVE_PACK_CMD, repoPath)
+func runSshCmd(session ssh.Session, command string, repoPath string) error {
+	cmd := exec.Command(command, repoPath)
 	sshOut := SshOut{s: session}
 
 	cmd.Stdout = sshOut
 	cmd.Stderr = session.Stderr()
 	cmd.Stdin = SshIn{session: session}
 
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-
-	return nil
+	return cmd.Run()
 }
 
-func runGitUploadPackCmd(session ssh.Session, repoPath string) error {
-	cmd := exec.Command(GIT_UPLOAD_PACK_CMD, repoPath)
-	sshOut := SshOut{s: session}
+type GitServer struct {
+	Addr       string
+	HostKeyPEM []byte
+	KeysStore  KeysStore
 
-	cmd.Stdout = sshOut
-	cmd.Stderr = session.Stderr()
-	cmd.Stdin = SshIn{session: session}
+	sshServer *ssh.Server
+}
 
-	if err := cmd.Run(); err != nil {
+type KeysStore interface {
+	Authenticate(key ssh.PublicKey) (*User, error)
+}
+
+type FileKeysStore struct {
+	ClientKeysPath string
+}
+
+type User struct {
+}
+
+func (keysStore *FileKeysStore) Authenticate(clientPublicKey ssh.PublicKey) (*User, error) {
+	clientKeysBytes, err := os.ReadFile(keysStore.ClientKeysPath)
+	if err != nil {
+		return nil, err
+	}
+	rawClientKeys := strings.Split(string(clientKeysBytes), "\n")
+
+	for idx := range rawClientKeys {
+		keyStr := rawClientKeys[idx]
+		if len(strings.TrimSpace(keyStr)) == 0 {
+			continue
+		}
+		publicKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(keyStr))
+		if err != nil {
+			return nil, err
+		}
+		if ssh.KeysEqual(clientPublicKey, publicKey) {
+			return &User{}, nil
+		}
+	}
+	return nil, nil
+}
+
+func (server *GitServer) Serve() error {
+	sshServer := &ssh.Server{
+		Addr:             server.Addr,
+		Handler:          server.SshSessionHandler,
+		PublicKeyHandler: server.AuthKeyHandler,
+	}
+
+	if err := sshServer.SetOption(ssh.HostKeyPEM(server.HostKeyPEM)); err != nil {
 		return err
 	}
 
-	return nil
+	return sshServer.ListenAndServe()
+}
+
+func (server *GitServer) AuthKeyHandler(ctx ssh.Context, key ssh.PublicKey) bool {
+	if user, err := server.KeysStore.Authenticate(key); err != nil {
+		fmt.Fprintf(os.Stderr, "Authenticate error: %v\n", err)
+		return false
+	} else if user == nil {
+		return false
+	}
+	return true
+}
+
+func (server *GitServer) SshSessionHandler(s ssh.Session) {
+	args, err := shlex.Split(s.RawCommand())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+
+	if args[0] == GIT_RECEIVE_PACK_CMD || args[0] == GIT_UPLOAD_PACK_CMD {
+		if err := runSshCmd(s, args[0], args[1]); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+	}
+}
+
+func NewGitServer(addr, hostKeyFilePath, clientKeysPath string) (*GitServer, error) {
+	hostKeyPEM, err := os.ReadFile(hostKeyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	keyStore := &FileKeysStore{
+		ClientKeysPath: clientKeysPath,
+	}
+
+	return &GitServer{
+		Addr:       ":2222",
+		KeysStore:  keyStore,
+		HostKeyPEM: hostKeyPEM,
+	}, nil
 }
 
 func main() {
-	ssh.Handle(func(s ssh.Session) {
-		fmt.Println("ssh raw command:", s.RawCommand())
+	server, err := NewGitServer(
+		":2222",
+		".ssh/host_key",
+		".ssh/client_keys",
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		args, err := shlex.Split(s.RawCommand())
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			return
-		}
-
-		if args[0] == GIT_RECEIVE_PACK_CMD {
-			if err := runGitReceivePackCmd(s, args[1]); err != nil {
-				fmt.Fprintln(os.Stderr, err.Error())
-			}
-		} else if args[0] == GIT_UPLOAD_PACK_CMD {
-			if err := runGitUploadPackCmd(s, args[1]); err != nil {
-				fmt.Fprintln(os.Stderr, err.Error())
-			}
-		}
-	})
-
-	log.Fatal(ssh.ListenAndServe(":2222", nil, ssh.HostKeyFile(".ssh/id_ed25519")))
+	log.Fatal(server.Serve())
 }
